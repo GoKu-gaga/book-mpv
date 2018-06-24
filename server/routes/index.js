4835c4856c5f7f9527200744b813521253bab7c1
yuanyican <max.yuan.ivan@gmail.com>
2018-06-24 17:31:08 +0800

    add book

--- server/routes/index.js
+++ server/routes/index.js
@@ -36,6 +36,8 @@ router.post('/message', controllers.message.post)
 
 router.get('/demo', controllers.demo)
 
+router.post('/addbook', controllers.addbook)
+
 router.get('/top', controllers.top)
 
 module.exports = router
